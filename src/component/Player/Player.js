5e6865b5a244b1c03424f237c4f959fe66376997
Sumaiya-Nawsheen <sumaiya.naw252@gmail.com>
2021-03-01 17:32:42 +0600

    react-bootstrap

--- src/component/Player/Player.js
+++ src/component/Player/Player.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import "./Player.css"
+import "./Player.css";
+import {Button, Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Player = (props) => {
     const {name, salary, img} = props.player;
     const handleAddPlayer = props.handleAddPlayer;
     return (
-        <div className="player-container">
-            <div className="player-detail">
-            <div>
-                <img src={img} alt=''/>
-            </div>
-            <div>
-            <h2>{name} </h2>
-            <p>${salary}</p>
-            <button onClick={()=> handleAddPlayer(props.player)}> Add Player</button>
-            </div>
-             </div>
-           
-        </div>
+
+<div className="container-fluid d-flex flex-row mb-3">
+<Card className="mb-3" style={{color:"#000", width:"18rem"}}>
+<Card.Img src={img}/> 
+<Card.Body>
+       <Card.Title>  <h2>{name} </h2> </Card.Title>
+    <Card.Text>
+      <p>Salary: ${salary}</p>
+      </Card.Text>
+      < Button variant="danger" onClick={()=> handleAddPlayer(props.player)}> Add Player</ Button>
+      </Card.Body>
+  </Card>
+  </div> 
     );
 };
-
+          
 export default Player;
